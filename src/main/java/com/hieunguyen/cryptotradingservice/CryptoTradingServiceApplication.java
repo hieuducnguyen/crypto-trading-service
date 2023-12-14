22e47b01c5e60d8835ec246f4dafc73f04db6854
HieuNguyen <hieu.nguyen@reviver.com>
2023-12-14 16:12:01 +0700

    InitSpringTemplateProject

---
+++ src/main/java/com/hieunguyen/cryptotradingservice/CryptoTradingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hieunguyen.cryptotradingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTradingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTradingServiceApplication.class, args);
+	}
+
+}
